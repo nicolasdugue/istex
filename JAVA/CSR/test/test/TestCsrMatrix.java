283c825e6b48a67bb047e4f2c66f0820527e063f
Nicolas Dugué <dugue.nic@gmail.com>
2015-09-30 10:00:48 +0200

    MAJ des tests pour la prise en compte des nrm, elm et idd

--- JAVA/CSR/test/test/TestCsrMatrix.java
+++ JAVA/CSR/test/test/TestCsrMatrix.java
@@ -6,11 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import io.reader.ArcsReader;
-import io.reader.IMatrixReader;
+import io.reader.interfaces.IMatrixReader;
 import model.cluster.Clustering;
 import model.matrix.CsrMatrix;
 import model.matrix.CsrMatrixClustered;
-import model.util.nuplet.PairI;
+import model.util.nuplet.PairF;
 
 public class TestCsrMatrix {
 
@@ -32,24 +32,24 @@ public class TestCsrMatrix {
 
 	@Test
 	public void testCsrMatrixMatrixReader() {
-		assertEquals(new PairI(0, 2), cm.getIinRows(0));
-		assertEquals(new PairI(3, 7), cm.getIinRows(1));
-		assertEquals(new PairI(0, 1), cm.getIinRows(2));
-		assertEquals(new PairI(1, 3), cm.getIinRows(3));
-		assertEquals(new PairI(3, 8), cm.getIinRows(4));
-		assertEquals(new PairI(2, 4), cm.getIinRows(5));
+		assertEquals(new PairF(0, 2f), cm.getIinRows(0));
+		assertEquals(new PairF(3, 7f), cm.getIinRows(1));
+		assertEquals(new PairF(0, 1f), cm.getIinRows(2));
+		assertEquals(new PairF(1, 3f), cm.getIinRows(3));
+		assertEquals(new PairF(3, 8f), cm.getIinRows(4));
+		assertEquals(new PairF(2, 4f), cm.getIinRows(5));
 
 		assertEquals(2, cm.getCumulativeRows(0));
 		assertEquals(4, cm.getCumulativeRows(1));
 		assertEquals(5, cm.getCumulativeRows(2));
 		assertEquals(6, cm.getCumulativeRows(3));
 
-		assertEquals(new PairI(0, 2), cm.getIinColumns(0));
-		assertEquals(new PairI(1, 1), cm.getIinColumns(1));
-		assertEquals(new PairI(1, 3), cm.getIinColumns(2));
-		assertEquals(new PairI(3, 4), cm.getIinColumns(3));
-		assertEquals(new PairI(0, 7), cm.getIinColumns(4));
-		assertEquals(new PairI(2, 8), cm.getIinColumns(5));
+		assertEquals(new PairF(0, 2f), cm.getIinColumns(0));
+		assertEquals(new PairF(1, 1f), cm.getIinColumns(1));
+		assertEquals(new PairF(1, 3f), cm.getIinColumns(2));
+		assertEquals(new PairF(3, 4f), cm.getIinColumns(3));
+		assertEquals(new PairF(0, 7f), cm.getIinColumns(4));
+		assertEquals(new PairF(2, 8f), cm.getIinColumns(5));
 
 		assertEquals(2, cm.getCumulativeColumns(0));
 		assertEquals(3, cm.getCumulativeColumns(1));
@@ -103,9 +103,9 @@ public class TestCsrMatrix {
 		assertEquals(1, cm.getRow(2).size());
 		assertEquals(1, cm.getRow(3).size());
 
-		assertTrue(cm.getRow(0).contains(new PairI(0, 2)) && cm.getRow(0).contains(new PairI(3, 7))
-				&& cm.getRow(1).contains(new PairI(0, 1)) && cm.getRow(1).contains(new PairI(1, 3))
-				&& cm.getRow(2).contains(new PairI(3, 8)) && cm.getRow(3).contains(new PairI(2, 4)));
+		assertTrue(cm.getRow(0).contains(new PairF(0, 2f)) && cm.getRow(0).contains(new PairF(3, 7f))
+				&& cm.getRow(1).contains(new PairF(0, 1f)) && cm.getRow(1).contains(new PairF(1, 3f))
+				&& cm.getRow(2).contains(new PairF(3, 8f)) && cm.getRow(3).contains(new PairF(2, 4f)));
 	}
 
 	public void getColumn() {
@@ -114,9 +114,9 @@ public class TestCsrMatrix {
 		assertEquals(1, cm.getColumn(2).size());
 		assertEquals(2, cm.getColumn(3).size());
 
-		assertTrue(cm.getColumn(0).contains(new PairI(0, 2)) && cm.getColumn(0).contains(new PairI(1, 1))
-				&& cm.getColumn(1).contains(new PairI(1, 3)) && cm.getColumn(2).contains(new PairI(3, 4))
-				&& cm.getColumn(3).contains(new PairI(0, 7)) && cm.getColumn(3).contains(new PairI(2, 8)));
+		assertTrue(cm.getColumn(0).contains(new PairF(0, 2f)) && cm.getColumn(0).contains(new PairF(1, 1f))
+				&& cm.getColumn(1).contains(new PairF(1, 3f)) && cm.getColumn(2).contains(new PairF(3, 4f))
+				&& cm.getColumn(3).contains(new PairF(0, 7f)) && cm.getColumn(3).contains(new PairF(2, 8f)));
 	}
 
 }
--- JAVA/CSR/test/test/TestMatrixReader.java
+++ JAVA/CSR/test/test/TestMatrixReader.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import io.reader.MatrixReader;
-import model.util.nuplet.PairI;
+import model.util.nuplet.PairF;
 
 public class TestMatrixReader {
 
@@ -20,27 +20,27 @@ public class TestMatrixReader {
 	@Test
 	public void testRead() {
 			//Rows
-			assertEquals(m.getXijRows(0, 0),(new PairI(0,2)));
-			assertEquals(m.getXijRows(0, 1),(new PairI(3,7)));
+			assertEquals(m.getXijRows(0, 0),(new PairF(0,2f)));
+			assertEquals(m.getXijRows(0, 1),(new PairF(3,7f)));
 			
-			assertEquals(m.getXijRows(1, 0),(new PairI(0,1)));
-			assertEquals(m.getXijRows(1, 1),(new PairI(1,3)));
+			assertEquals(m.getXijRows(1, 0),(new PairF(0,1f)));
+			assertEquals(m.getXijRows(1, 1),(new PairF(1,3f)));
 			
-			assertEquals(m.getXijRows(2, 0),(new PairI(3,8)));
+			assertEquals(m.getXijRows(2, 0),(new PairF(3,8f)));
 			
-			assertEquals(m.getXijRows(3, 0),(new PairI(2,4)));
+			assertEquals(m.getXijRows(3, 0),(new PairF(2,4f)));
 			
 			
 			//Columns
-			assertEquals(m.getXijColumns(0, 0),(new PairI(0,2)));
-			assertEquals(m.getXijColumns(0, 1),(new PairI(1,1)));
+			assertEquals(m.getXijColumns(0, 0),(new PairF(0,2f)));
+			assertEquals(m.getXijColumns(0, 1),(new PairF(1,1f)));
 			
-			assertEquals(m.getXijColumns(1, 0),(new PairI(1,3)));
+			assertEquals(m.getXijColumns(1, 0),(new PairF(1,3f)));
 			
-			assertEquals(m.getXijColumns(2, 0),(new PairI(3,4)));
+			assertEquals(m.getXijColumns(2, 0),(new PairF(3,4f)));
 			
-			assertEquals(m.getXijColumns(3, 0),(new PairI(0,7)));
-			assertEquals(m.getXijColumns(3, 1),(new PairI(2,8)));
+			assertEquals(m.getXijColumns(3, 0),(new PairF(0,7f)));
+			assertEquals(m.getXijColumns(3, 1),(new PairF(2,8f)));
 			
 			//Descriptors
 			assertEquals(m.getNb_columns(),4);
